015b84c35c52bc96b888c44756063f3f2dcb45f1
Mihail Piakhota <piakhota@bk.ru>
2022-07-07 00:08:02 +0400

    HW05 tasks3-4

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -29,14 +29,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles({"postgres", "datajpa"})
-public class MealServiceTest extends AbstractBaseServiceTest {
+public class MealServiceTest extends BaseServiceTest{
     private static final Logger log = getLogger("result");
 
     private static final StringBuilder results = new StringBuilder();
